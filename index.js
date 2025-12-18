2c6e581ba269627891c742527caaf4843c1b3f69
ashtophoenix <166555474+ashtophoenix@users.noreply.github.com>
2025-12-18 13:33:18 +0200

    Added html and js

---
+++ index.js
@@ -0,0 +1,50 @@
+let adjective = "happy";
+let noun = "cat";
+let verb = "sleeping";
+let place = "park";
+let adjective2 = "bright";
+let noun2 = "fish";
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " " +
+  "who loved to eat " +
+  noun2 +
+  "." +
+  " The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+adjective = "sad";
+noun = "dog";
+verb = "barking";
+place = "river";
+adjective2 = "scary";
+noun2 = "beans";
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " " +
+  "who loved to eat " +
+  noun2 +
+  "." +
+  " The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story: " + secondStory);
